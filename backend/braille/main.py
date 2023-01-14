4e4b61a6dec03eb2a90cd96acfee56aab3be0efe
Hong-ut <105889282+Hong-ut@users.noreply.github.com>
2023-01-14 13:37:45 -0500

    Update main.py

--- backend/braille/main.py
+++ backend/braille/main.py
@@ -1,2 +1,22 @@
 import braille
-braille.textToBraille('string')
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/textToBraille/<string:text>', methods=['GET'])
+def text_to_braille(text):
+    text_to_braille = braille.textToBraille(text)
+    return jsonify({'message' : text_to_braille})
+
+@app.route('/', methods=['GET'])
+def braille_to_text(): # braille IMAGE to text
+    return jsonify({'message' : text_to_braille})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
