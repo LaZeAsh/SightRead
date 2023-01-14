832163178e86f1fb55c38ca293cf86ca0ab647dc
Hong-ut <105889282+Hong-ut@users.noreply.github.com>
2023-01-14 16:58:48 -0500

    Update main.py

--- backend/braille/main.py
+++ backend/braille/main.py
@@ -2,6 +2,7 @@ import braille
 from flask import Flask
 from flask import jsonify
 from flask import request
+from brailleImage import braille_image_to_english
 
 app = Flask(__name__)
 
@@ -10,24 +11,24 @@ def text_to_braille(text):
     text_to_braille = braille.textToBraille(text)
     return jsonify({'message' : text_to_braille})
 
-@app.route('/brailleToText/<string:text>', methods=['GET'])
-def braille_to_text(text):
+# Convert Braille STRING -> TEXT
+@app.route('/brailleTextToText/<string:text>', methods=['GET'])
+def braille_text_to_text(text):
     translated_text = braille.brailleToTextArray(text)
     return jsonify({'message' : translated_text})
 
-@app.route('/brailleToText/<string:link>', methods=['GET'])
-def get_image_text(link):
-    text = braille.imageToText(link)
+# any ImageLink -> TEXT
+@app.route('/imageToText/<string:url>', methods=['GET'])
+def get_image_text(url):
+    text = braille.imageToText(url)
     return jsonify({'message' : text})
 
-@app.route('/brailleToText/<string:link>', methods=['GET'])
-def get_image_text(link):
-    text = braille.imageToBraille(link)
-    return jsonify({'message' : text})
+# # Braille Image -> TEXT
+# @app.route('/brailleImageToText/<string:url>', methods=['GET'])
+# def braille_image_to_text(url):
+#     text = braille_image_to_english(url)
+#     return jsonify({'message' : text})
 
-@app.route('/', methods=['GET'])
-def hello():
-    return "Hello"
 
 if __name__ == "__main__":
     app.run(debug=True)
