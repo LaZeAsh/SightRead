aa64894a4700e1272148800026db334e2592ed24
Caroline Serapio <105842563+CarSerapio@users.noreply.github.com>
2023-01-14 15:59:55 -0500

    Add files via upload

---
+++ frontend-ui/Braille-Interpreter/Braille-Interpreter/Braille_InterpreterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Braille_InterpreterApp.swift
+//  Braille-Interpreter
+//
+//  Created by Chloe Serapio on 2023-01-14.
+//
+
+import SwiftUI
+
+@main
+struct Braille_InterpreterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Title()
+        }
+    }
+}
