d26500a170b2b7e3730c52ddaed68e574aa096d1
JaakLipp <lippert.22j@gmail.com>
2023-01-14 15:18:50 -0500

    image support in flask

--- backend/braille/main.py
+++ backend/braille/main.py
@@ -3,10 +3,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
-
-
 app = Flask(__name__)
-
 
 @app.route('/textToBraille/<string:text>', methods=['GET'])
 def text_to_braille(text):
@@ -18,11 +15,19 @@ def braille_to_text(text):
     translated_text = braille.brailleToTextArray(text)
     return jsonify({'message' : translated_text})
 
+@app.route('/brailleToText/<string:link>', methods=['GET'])
+def get_image_text(link):
+    text = braille.imageToText(link)
+    return jsonify({'message' : text})
+
+@app.route('/brailleToText/<string:link>', methods=['GET'])
+def get_image_text(link):
+    text = braille.imageToBraille(link)
+    return jsonify({'message' : text})
+
 @app.route('/', methods=['GET'])
 def hello():
     return "Hello"
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
