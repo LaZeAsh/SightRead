11f957ba2418d9fd96ec5a4e12a46dee7662c80c
Caroline Serapio <105842563+CarSerapio@users.noreply.github.com>
2023-01-14 22:45:43 -0500

    removing unnecessary files

---
+++ frontend/Braille-Interpreter/Braille_InterpreterApp.swift
@@ -0,0 +1,15 @@
+//
+//  Braille_InterpreterApp.swift
+//  Braille-Interpreter
+//
+
+import SwiftUI
+
+@main
+struct Braille_InterpreterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Title()
+        }
+    }
+}
