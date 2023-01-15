e547b891fb6edbcf00e04d9e926de11b52e3d477
Caroline Serapio <105842563+CarSerapio@users.noreply.github.com>
2023-01-15 01:13:02 -0500

    separated views

---
+++ frontend/Braille-Interpreter/Views/InterpretedBraille.swift
@@ -0,0 +1,69 @@
+//
+//  InterpretedText.swift
+//  Braille-Interpreter
+//
+
+import SwiftUI
+
+struct InterpretedBraille: View {
+    
+    let text: String = "⠠⠇⠕⠗⠑⠍ ⠊⠏⠎⠥⠍ ⠙⠕⠇⠕⠗ ⠎⠊⠞ ⠁⠍⠑⠞⠂ ⠉⠕⠝⠎⠑⠉⠞⠑⠞⠥⠗ ⠁⠙⠊⠏⠊⠎⠉⠊⠝⠛ ⠑⠇⠊⠞⠂ ⠎⠑⠙ ⠙⠕ ⠑⠊⠥⠎⠍⠕⠙ ⠞⠑⠍⠏⠕⠗ ⠊⠝⠉⠊⠙⠊⠙⠥⠝⠞ ⠥⠞ ⠇⠁⠃⠕⠗⠑ ⠑⠞ ⠙⠕⠇⠕⠗⠑ ⠍⠁⠛⠝⠁ ⠁⠇⠊⠟⠥⠁. ⠠⠥⠞ ⠑⠝⠊⠍ ⠼⠁⠙ ⠍⠊⠝⠊⠍ ⠧⠑⠝⠊⠁⠍⠂ ⠟⠥⠊⠎ ⠝⠕⠎⠞⠗⠥⠙ ⠑⠭⠑⠗⠉⠊⠞⠁⠞⠊⠕⠝ ⠥⠇⠇⠁⠍⠉⠕ ⠇⠁⠃⠕⠗⠊⠎ ⠝⠊⠎⠊ ⠥⠞ ⠁⠇⠊⠟⠥⠊⠏ ⠑⠭ ⠼⠑⠁ ⠉⠕⠍⠍⠕⠙⠕ ⠉⠕⠝⠎⠑⠟⠥⠁⠞. ⠠⠙⠥⠊⠎ ⠁⠥⠞⠑ ⠊⠗⠥⠗⠑ ⠙⠕⠇⠕⠗ ⠊⠝ ⠗⠑⠏⠗⠑⠓⠑⠝⠙⠑⠗⠊⠞ ⠊⠝ ⠧⠕⠇⠥⠏⠞⠁⠞⠑ ⠧⠑⠇⠊⠞ ⠑⠎⠎⠑ ⠉⠊⠇⠇⠥⠍ ⠙⠕⠇⠕⠗⠑ ⠑⠥ ⠋⠥⠛⠊⠁⠞ ⠝⠥⠇⠇⠁ ⠏⠁⠗⠊⠁⠞⠥⠗. ⠠⠑⠭⠉⠑⠏⠞⠑⠥⠗ ⠎⠊⠝⠞ ⠕⠉⠉⠁⠑⠉⠁⠞ ⠉⠥⠏⠊⠙⠁⠞⠁⠞ ⠝⠕⠝ ⠏⠗⠕⠊⠙⠑⠝⠞⠂ ⠎⠥⠝⠞ ⠊⠝ ⠉⠥⠇⠏⠁ ⠟⠥⠊ ⠕⠋⠋⠊⠉⠊⠁ ⠙⠑⠎⠑⠗⠥⠝⠞ ⠍⠕⠇⠇⠊⠞ ⠁⠝⠊⠍ ⠼⠊⠙ ⠑⠎⠞ ⠇⠁⠃⠕⠗⠥⠍"
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text("Your interpreted text")
+                    .font(.title2.weight(.light))
+            }
+            .foregroundColor(.white)
+            .padding()
+            Spacer()
+            ScrollView {
+                Text(self.text)
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 4, style: .continuous)
+                            .fill(Color(.systemFill))
+                    }
+                    .foregroundColor(.white)
+ 
+                
+            }
+            VStack(spacing: 10) {
+                NavigationLink(destination: Title(), label: {
+                    HStack(alignment: .firstTextBaseline) {
+                        Image(systemName: "house")
+                            .imageScale(.medium)
+                            .symbolRenderingMode(.monochrome)
+                        Text("Return home")
+                    }
+                    .font(.body.weight(.medium))
+                    .padding(.vertical, 16)
+                    .frame(maxWidth: .infinity)
+                    .clipped()
+                    .foregroundColor(.green)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .stroke(.clear.opacity(0.25), lineWidth: 0)
+                            .background(RoundedRectangle(cornerRadius: 10, style: .continuous).fill(.yellow.opacity(0.1)))
+                    }
+                })
+            }
+            .padding()
+        }
+        .frame(maxWidth: .infinity)
+        .clipped()
+        .padding(.top, 53)
+        .padding(.bottom, 0)
+        .background {
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .fill(.indigo)
+        }.ignoresSafeArea()
+    }
+}
+
+struct InterpretedBraille_Previews: PreviewProvider {
+    static var previews: some View {
+        InterpretedBraille()
+    }
+}
---
+++ frontend/Braille-Interpreter/Views/TextToBraille.swift
@@ -0,0 +1,62 @@
+//
+//  TextToBraille.swift
+//  Braille-Interpreter
+//
+
+import SwiftUI
+
+struct TextToBraille: View {
+    
+    @State var text: String = ""
+
+    var body: some View {
+        VStack {
+            VStack {
+                Text("Text to braille")
+                    .font(.title2.weight(.light))
+            }
+            .foregroundColor(.white)
+            .padding()
+            Spacer()
+            
+            TextEditor(text: $text)
+            
+    
+            VStack(spacing: 10) {
+                NavigationLink(destination: InterpretedBraille(), label: {
+                    HStack(alignment: .firstTextBaseline) {
+                        Image(systemName: "arrow.forward")
+                            .imageScale(.medium)
+                            .symbolRenderingMode(.monochrome)
+                        Text("Convert to braille")
+                    }
+                    .font(.body.weight(.medium))
+                    .padding(.vertical, 16)
+                    .frame(maxWidth: .infinity)
+                    .clipped()
+                    .foregroundColor(.green)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .stroke(.clear.opacity(0.25), lineWidth: 0)
+                            .background(RoundedRectangle(cornerRadius: 10, style: .continuous).fill(.yellow.opacity(0.1)))
+                    }
+                })
+            }
+            .padding()
+        }
+        .frame(maxWidth: .infinity)
+        .clipped()
+        .padding(.top, 53)
+        .padding(.bottom, 0)
+        .background {
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .fill(.indigo)
+        }.ignoresSafeArea()
+    }
+}
+
+struct TextToBraille_Previews: PreviewProvider {
+    static var previews: some View {
+        TextToBraille(text: "")
+    }
+}
